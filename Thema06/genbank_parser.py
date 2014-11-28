f427115292323256807075f35d80838cd2194c5f
harmbrugge <harmbrugge@gmail.com>
2014-11-28 14:40:34 +0100

    added Chromosome parser

--- Thema06/genbank_parser.py
+++ Thema06/genbank_parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import re
 
+
 class GenBank:
 
     def __init__(self, file):
@@ -18,9 +19,35 @@ class GenBank:
         handle.close()     
 
     def make_chromosome(self):
-        pass
+        definition = re.search('DEFINITION\s*(.*),\schromosome\s(\d|\w{1,2})', self.seq)
+        organism = definition.group(1)
+        chromosome_id = definition.group(2)
+
+        seq = re.search('ORIGIN(.*)//', self.seq, re.DOTALL).group(1)
+        seq = re.sub('\n|\s|\d', '', seq)
+
+        return Chromosome(seq, chromosome_id, organism)
 
     def make_genes(self):
         pass
 
-print(GenBank('chromosome_1.gb'))
+
+class Chromosome:
+
+    def __init__(self, seq, chromosome_id, organism):
+        self.chromosome_id = chromosome_id
+        self.seq = seq
+        self.organism = organism
+
+
+class Gene:
+    def __init__(self, gene_id, chromosome_id, exons, protein, protein_id):
+
+        pass
+
+genBank = GenBank('chromosome_1.gb')
+
+chromosome1 = genBank.make_chromosome()
+print(chromosome1.chromosome_id)
+print(chromosome1.organism)
+print(chromosome1.seq)
