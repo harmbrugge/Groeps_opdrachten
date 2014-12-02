23b7833703274be1e842b6b1142092f2be513fdb
OlivierBakker <olivierbakker25@gmail>
2014-12-02 10:52:05 +0100

    added folder support and fixed the name crreation for the mt chromosome(may need zome work)

--- Thema06/genbank_parser.py
+++ Thema06/genbank_parser.py
@@ -5,8 +5,21 @@ import os
 
 # TODO error/exception handeling!!
 class GenBank:
+    """
+    This class handles the interactions pretaining to the genbank file.
+    It's also responsible for creating the chromosome and gene objects.
+    The data from the genbank file is stored in 1 large string, so this
+    script is non streaming. We opted for this option because we thought
+    the code would more flexible this way.
+    """
 
     def __init__(self, file):
+        """
+        file_string:        The string containing the entire genbank file.
+        chromosome:         The chromosome object for the genbank file.
+        genes:              A list of gene objects for the chromosome
+        :param file:        The path + filename for the genbank file one wishes to excecute.
+        """
 
         self.file_string = str()
         self.file = file
@@ -18,6 +31,11 @@ class GenBank:
         return self.file_string
 
     def _read_file(self):
+        """
+        This method is only calleble by the class itself.
+        It opens the genbank file for the current path + filename
+        and sets the file_string for the current genbank file.
+        """
 
         handle = open(self.file, 'r')
         var = handle.readlines()
@@ -25,13 +43,20 @@ class GenBank:
         handle.close()     
 
     def make_chromosome(self):
+        """
+        This method handles the creation of the chromosome object.
+        It accomplishes this using regex.
+
+        :return: A chromosome object.
+        """
 
         try:
-            # So the mt chromosome is picked up.
+            # The regex code that searches for the mt chromosome.
             definition = re.search('DEFINITION\s*(.*)\s(mitochondrion)', self.file_string)
             if definition:
                 definition = definition
             else:
+                # If no mitochondrion is present.
                 definition = re.search('DEFINITION\s*(.*)\schromosome\s(\d*|\w*)', self.file_string)
 
             organism = definition.group(1).replace(",", "")
@@ -48,14 +73,21 @@ class GenBank:
         return self.chromosome
 
     def make_genes(self):
+        """
+        This method handles the creation of the gene objects.
+        :return: A list of gene objects.
+        """
 
+        # The regex to get all of the coding DNA seqeunces.
         cds_info = re.findall('CDS(.+?)/translation', self.file_string, re.DOTALL)
+        # Creates the dict for the creation of an reverse complement.
         trans_table = str.maketrans("atcg", "tagc")
         gene_list = list()
         strand = str()
 
         for cur_cds in cds_info:
 
+            # The regex to get all of the info from an cds.
             exon_seqs = list()
             exon_regions = re.findall('.(\d*)\.\.>?(\d*)', cur_cds, re.DOTALL)
             gene_id = re.search('/db_xref="GeneID:(\d*)', cur_cds).group(1)
@@ -66,23 +98,26 @@ class GenBank:
 
             for i, exon in enumerate(exon_regions):
 
+                # If a strand is a complement excecute this.
                 if re.match('.*complement', cur_cds):
                     strand = '-'
-                    gen_seq = self.chromosome.seq[int(exon[0]):int(exon[1])][::-1]
-                    gen_seq = gen_seq.translate(trans_table)
+                    gen_seq = self.chromosome.seq[int(exon[0]):int(exon[1])][::-1]  # Reverse the strand.
+                    gen_seq = gen_seq.translate(trans_table)  # Make the stand complement.
                     exon_seqs.append(gen_seq)
 
                 else:
                     strand = '+'
-                    gen_seq = self.chromosome.seq[int(exon[0])-1:int(exon[1])-1]
+                    gen_seq = self.chromosome.seq[int(exon[0])-1:int(exon[1])-1]  # Get the strand
                     exon_seqs.append(gen_seq)
 
+            # Create the seqeunce string for the gene.
             if strand == '-':
-                exon_seqs = ''.join(exon_seqs[::-1])
+                exon_seqs = ''.join(exon_seqs[::-1])  # If the strand is negative it needs to be joined in reverse
 
             elif strand == '+':
-                exon_seqs = ''.join(exon_seqs)
+                exon_seqs = ''.join(exon_seqs) # Join the strands normally
 
+            # Creation of the gene objects.
             gene_list.append(Gene(gene_id,
                                   strand,
                                   exon_regions,
@@ -96,6 +131,10 @@ class GenBank:
 
 
 class Chromosome:
+    """
+    The class that servers as a framework to store the chromosome
+    data.
+    """
 
     def __init__(self, seq, chromosome_id, organism):
         self.chromosome_id = chromosome_id
@@ -105,6 +144,10 @@ class Chromosome:
 
 
 class Gene:
+    """
+    The class that servers as a framework to store the gene
+    data.
+    """
 
     def __init__(self, gene_id, strand, exon_regions, exon_seqs, protein, protein_id, chromosome):
         self.gene_id = gene_id
@@ -118,25 +161,43 @@ class Gene:
 
 
 class FastaWriter:
+    """
+    The class that handles the creation of the fasta files.
+    """
 
-    # TODO create innit containing the path to output dir.
     @staticmethod
     def write_genes(genes, output_dir):
-        filename = output_dir + 'chromosome-' + genes[0].chromosome_id + '_' + genes[0].organism.replace(' ', '-') + '_genes'
+        """
+        This method creates the gene fasta file
+        :param genes: A list of Gene objects
+        :param output_dir: The path of the output dir
+        """
+        # The creation of the filename .
+        filename = output_dir + 'chromosome-' \
+                              + genes[0].chromosome_id \
+                              + '_' + genes[0].organism.replace(' ', '-')\
+                              + '_genes'
 
         if os.path.exists(filename):
             print('Fasta file', filename, 'exists already')
             # TODO build proper naming and path selection! (kinda done)
 
         else:
+            # Open the file.
             file = open(filename, 'w')
             for gene in genes:
 
-                fasta_id = '>gene_' + str(gene.gene_id) + '|' + str(gene.strand) + '|' + str(gene.protein) + '\n'
+                # Creation of the fasta id.
+                fasta_id = '>gene_' \
+                           + str(gene.gene_id) \
+                           + '|' + str(gene.strand) \
+                           + '|' + str(gene.protein) + '\n'
+
                 file.write(fasta_id)
                 gen_seq = gene.exon_seqs
                 seq_to_write = list()
 
+                # Add a newline char every 75 chars
                 for i in range(0, len(gen_seq), 75):
                     seq_to_write.append(gen_seq[i:i+75])
                 file.write('\n'.join(seq_to_write) + '\n\n')
@@ -145,17 +206,26 @@ class FastaWriter:
 
     @staticmethod
     def write_chromosome(chromosome, output_dir):
-        filename = output_dir + 'chromosome_' + str(chromosome.chromosome_id) + '_' + str(chromosome.organism.replace(' ', '-'))
 
+        # The creation of the filename .
+        filename = output_dir \
+                + 'chromosome_' \
+                + str(chromosome.chromosome_id) \
+                + '_' + str(chromosome.organism.replace(' ', '-'))
+
+        # Check if the file exists
         if os.path.exists(filename):
             print('Chromosome', filename, 'file exists already')
             # TODO build proper naming and path selection! (kinda done)
 
         else:
             file = open(filename, 'w')
+
+            # Creation of the fasta id.
             fasta_id = '>chromosome_' + str(chromosome.chromosome_id) + '|' + str(chromosome.organism) + '\n'
             file.write(fasta_id)
 
+            # Add a newline char every 75 chars
             seq_to_write = []
             for i in range(0, len(chromosome.seq), 75):
                 seq_to_write.append(chromosome.seq[i:i+75])
