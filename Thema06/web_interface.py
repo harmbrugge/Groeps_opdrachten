6960240f35165020a788ae33c8b127d647b97d4f
harmbrugge <harmbrugge@gmail.com>
2014-12-04 10:27:31 +0100

    Code opgeschoond

--- Thema06/web_interface.py
+++ Thema06/web_interface.py
@@ -11,40 +11,36 @@ import datetime
 class TarFile:
 
     @staticmethod
-    def get_tarfile(form):
+    def get_tarfile(filefield):
+        if not isinstance(filefield, list):
+            filefield = [filefield]
 
-        if 'file' in form.keys():
-            filefield = form['file']
+        file_list = list()
 
-            if not isinstance(filefield, list):
-                filefield = [filefield]
+        for fileitem in filefield:
+            file = fileitem.file.read()
+            file = file.decode("utf-8")
 
-            file_list = list()
+            genbank = genbank_parser.GenBank(file)
+            chromosome = genbank.make_chromosome()
+            chromosome.genes = genbank.make_genes()
+            gene = genbank.make_genes()
 
-            for fileitem in filefield:
-                file = fileitem.file.read()
-                file = file.decode("utf-8")
+            chromosome_file = genbank_parser.FastaWriter.get_chromosome_string(chromosome)
+            gene_file = genbank_parser.FastaWriter.get_gene_string(gene)
+            file_list.append(chromosome_file)
+            file_list.append(gene_file)
 
-                genbank = genbank_parser.GenBank(file)
-                chromosome = genbank.make_chromosome()
-                chromosome.genes = genbank.make_genes()
-                gene = genbank.make_genes()
+        output_filename = 'file/' + str(datetime.datetime.now()) + '_parsed-fasta-files.tar.gz'
 
-                chromosome_file = genbank_parser.FastaWriter.get_chromosome_string(chromosome)
-                gene_file = genbank_parser.FastaWriter.get_gene_string(gene)
-                file_list.append(chromosome_file)
-                file_list.append(gene_file)
+        tar = tarfile.open(output_filename, "w:gz")
+        for x in file_list:
+            tarinfo = tarfile.TarInfo(x[1])
+            tarinfo.size = len(x[0])
+            tar.addfile(tarinfo, io.BytesIO(x[0].encode('utf8')))
+        tar.close()
 
-            output_filename = 'file/' + str(datetime.datetime.now()) + '_parsed-fasta-files.tar.gz'
-
-            tar = tarfile.open(output_filename, "w:gz")
-            for x in file_list:
-                tarinfo = tarfile.TarInfo(x[1])
-                tarinfo.size = len(x[0])
-                tar.addfile(tarinfo, io.BytesIO(x[0].encode('utf8')))
-            tar.close()
-
-            print('<script>window.open("' + output_filename + '");</script>')
+        print('<script>window.open("' + output_filename + '");</script>')
 
 
 def main():
@@ -61,13 +57,16 @@ def main():
     print()
 
     print(html.get_header("GenBank parser"))
-    TarFile.get_tarfile(form)
 
     print('<form role="form" enctype="multipart/form-data" action=web_interface.py method="post">')
     print('<div class="form-group"><label for="file">File: </label>'
           '<input id="file" type="file" name="file" multiple=""></div>')
     print('<button class="btn btn-default" type="submit" value="Upload">Submit</button>')
     print('</form>')
+
+    if 'file' in form.keys():
+        filefield = form['file']
+        TarFile.get_tarfile(filefield)
 
     # End with footer
     print(html.get_footer())
