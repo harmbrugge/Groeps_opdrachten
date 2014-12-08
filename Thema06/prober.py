cb507c73bc2c258d17563dcdda8378bc461bf7eb
harmbrugge <harmbrugge@gmail.com>
2014-12-08 17:26:24 +0100

    Prober geowned

--- Thema06/prober.py
+++ Thema06/prober.py
@@ -11,31 +11,38 @@ class Prober:
     def make_probes(self, genes):
         trans_table = str.maketrans("atcg", "tagc")
 
-        for gene in genes:
+        repeat_single_nuc = re.compile(r'(.)\1{3}')
+        repeat_di_nuc = re.compile(r'(.{2})\1{2}')
+
+        #for gene in genes:
             #gene.exon_seqs = gene.exon_seqs.translate(trans_table)[::-1]
+        gene = genes[0]
 
-            for i in range(0, len(gene.exon_seqs)-20):
-                cur_probe = gene.exon_seqs[i:i+20]
-                # print(cur_probe)
+        for i in range(0, len(gene.exon_seqs)-20):
+            cur_probe_id = i
+            cur_probe = gene.exon_seqs[i:i+20]
 
-                if not re.match('(.)\\1{3}', cur_probe):
+            if not re.search(repeat_single_nuc, cur_probe):
+                if not re.search(repeat_di_nuc, cur_probe):
+                    # Pak alleen het gebied van 5 + 3(gap)
+                    hairpin_domain = cur_probe[8:]
 
-                    if not re.match('(.{2})\\1{3}', cur_probe):
-                        rev_cur_probe = cur_probe.translate(trans_table)[::-1]
-                        #comp_list = [rev_cur_probe[y:y+5] for y in range(0, int(len(rev_cur_probe)/2))]
+                    # Pak alle mogelijke sequenties van 5 in hairpin_domain
+                    for y in range(0, len(hairpin_domain)-5):
+                        hairpin_seq = hairpin_domain[y:y+5]
 
-                        for y in range(0, int(len(rev_cur_probe)/2)):
-                            comp = rev_cur_probe[y:y+5]
-                            hairpin_seq_pos = cur_probe.find(comp, y+8, len(cur_probe))
-                            if hairpin_seq_pos != -1:
-                                print(comp)
-                                print(comp.translate(trans_table)[::-1])
-                                print(cur_probe)
-                            # hairpin_comp_seq = cur_probe.find(comp.translate(trans_table)[::-1], 0, len(cur_probe))
-                            # if hairpin_seq_pos + 8 < hairpin_comp_seq and hairpin_seq_pos != -1 and hairpin_comp_seq != -1:
+                        # Maak de sequenties reverse complement
+                        hairpin_seq_rev = hairpin_seq[::-1]
+                        hairpin_seq_rev_com = hairpin_seq_rev.translate(trans_table)
 
-
-
+                        # Zoek op de probe naar de sequentie rekening houdens met eindlocatie
+                        hairpin_seq_pos = cur_probe.find(hairpin_seq_rev_com, 0, 5+y)
+                        if hairpin_seq_pos != -1:
+                            print(cur_probe_id)
+                            print(cur_probe)
+                            print(hairpin_seq_rev_com)
+                            print()
+                            print('--------------')
 
 
 def main():
