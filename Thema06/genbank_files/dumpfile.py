e62fb342eabe99e2b3e8420366f58c1c90d4f772
root <olivierbakker25@gmail.com>
2014-12-20 18:44:53 +0100

    started efficientie

--- Thema06/genbank_files/dumpfile.py
+++ Thema06/genbank_files/dumpfile.py
@@ -21,3 +21,30 @@ elif not repeat_len % 2:
         skip_value = (di_repeat_positions[0] + 2)
 
     i += int(skip_value)
+
+
+# di_repeat_positions = di_search.span()
+# repeat_len = di_repeat_positions[1] - di_repeat_positions[0]
+# if not repeat_len % 3:
+#
+#     repeat_count = (repeat_len/3) - self.nr_nuc_di_repeat
+#
+#     if repeat_count > 0:
+#         skip_value = (di_repeat_positions[0] + repeat_count*3)
+#     else:
+#         skip_value = (di_repeat_positions[0] + 3)
+#     i += int(skip_value)
+#
+# elif not repeat_len % 2:
+#
+#     repeat_count = (repeat_len/2) - self.nr_nuc_di_repeat
+#     if repeat_count > 0:
+#         skip_value = (di_repeat_positions[0] + repeat_count*2)
+#     else:
+#         skip_value = (di_repeat_positions[0] + 2)
+#
+#     i += int(skip_value)
+
+
+skip_value = mono_repeat_positions[1] - self.nr_nuc_mono_repeat
+i += (skip_value-1)
--- Thema06/prober.py
+++ Thema06/prober.py
@@ -105,36 +105,12 @@ class Prober:
                     else:
                         di_repeat_positions = di_search.span()
                         i += di_repeat_positions[0]
-                        # di_repeat_positions = di_search.span()
-                        # repeat_len = di_repeat_positions[1] - di_repeat_positions[0]
-                        # if not repeat_len % 3:
-                        #
-                        #     repeat_count = (repeat_len/3) - self.nr_nuc_di_repeat
-                        #
-                        #     if repeat_count > 0:
-                        #         skip_value = (di_repeat_positions[0] + repeat_count*3)
-                        #     else:
-                        #         skip_value = (di_repeat_positions[0] + 3)
-                        #     i += int(skip_value)
-                        #
-                        # elif not repeat_len % 2:
-                        #
-                        #     repeat_count = (repeat_len/2) - self.nr_nuc_di_repeat
-                        #     if repeat_count > 0:
-                        #         skip_value = (di_repeat_positions[0] + repeat_count*2)
-                        #     else:
-                        #         skip_value = (di_repeat_positions[0] + 2)
-                        #
-                        #     i += int(skip_value)
-
                         # testen of deze count klopt of +1 moet zijn
                         di_count += di_repeat_positions[0]
                         di_time_list.append(time.time()-start_time)
                 else:
                     mono_repeat_positions = mono_search.span()
-                    #i += mono_repeat_positions[0]
-                    skip_value = mono_repeat_positions[1] - self.nr_nuc_mono_repeat
-                    i += (skip_value-1)
+                    i += mono_repeat_positions[0]
                     # testen of deze count klopt of +1 moet zijn
                     mono_count += mono_repeat_positions[0]
                     mono_time_list.append(time.time()-start_time)
@@ -190,7 +166,7 @@ def main():
     chromosome_list = list()
 
     # loop over gbk files genbank dir
-    for file in glob.glob(os.path.join('genbank_files/Plasmodium/', '*.gbk')):
+    for file in glob.glob(os.path.join('genbank_files/Plasmodium', '*.gbk')):
         # read genbank file
 
         try:
@@ -217,7 +193,7 @@ def main():
         chromosome_list.append(chromosome)
         print('Done with chromsome:', chromosome.chromosome_id)
         print(sum(p_list))
-        break
+
 
     # # open a DB connection
     # database = database_functions.Database()
